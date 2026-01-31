77930c5bc8167abb2b0dfffeeefb376c367acd98
Akyltai <akyltai.koshkarov.2411@gmail.com>
2026-02-01 00:08:56 +0500

    Preparation for the merger

---
+++ Practice1/Data Types_.py
@@ -0,0 +1,10 @@
+a = None
+b = [5,8,9,1]
+c = {
+    6:"5",
+    "dr":"it",
+    True:"true"
+    }
+d = (4,5,6,78,)
+e = 'just string'
+print(a,b,c,d,e,sep=" ")
---
+++ Practice1/Numbers_.py
@@ -0,0 +1,15 @@
+a = 3
+b = 3.4
+c = 1j
+d = -3
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+t = int("6")
+v = complex("9j")
+f = float("5.9")
+
+print(t,v,f,sep=" ")
